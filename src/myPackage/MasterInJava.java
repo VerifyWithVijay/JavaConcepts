e62f07f435fd34a3b9bee19c1109472dacefa3c4
TanyaSaha <ravindra.kamisetty@gmail.com>
2025-09-17 09:46:37 +0530

    changes added

--- src/myPackage/MasterInJava.java
+++ src/myPackage/MasterInJava.java
@@ -1,9 +1,10 @@
 package myPackage;
 
-public class JavaBasics {
+public class MasterInJava {
 
     public static void main(String[] args) {
         System.out.println("Hi");
         System.out.println("Helloo..");
+        System.out.println("checking the code");
     }
 }
